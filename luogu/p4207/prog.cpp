b8fa8381a6b8e465a42c9139c1382898086c1b9e
Littlelu <3287597970@qq.com>
2023-10-04 21:33:01 +0800

    Add the code of p4207.

--- luogu/p4207/prog.cpp
+++ luogu/p4207/prog.cpp
@@ -4,26 +4,55 @@
 #include <algorithm>
 using namespace std;
 const static int SIZE=510;
+const static double EPS=1e-7;
+// some initial data (in the problem).
+int n;
+double alpha,h[SIZE],r[SIZE];
+double pjx[SIZE];
+double ans=0;
+
+// true means the circle, false means the line (function).
+bool type[SIZE];
+// board of functions, for final INTEGRATION.
+double bod[SIZE];
+// if we uses circle function y=sqrt(v^2-(x-p)^2), we must record v and the p.
+double v[SIZE],p[SIZE];
+// if we uses the line function y=kx+b, we must record k and b.
+double k[SIZE],b[SIZE];
+// number of functions(in boards).
+int cnt;
+
+// square of a number x.
 double sqr(double x){
     return x*x;
 }
+
+// circle function (at line 18 comment).
+double cirf(double v,double p,double x){
+    return sqrt(v*v-sqr(x-p));
+}
+
+// use auto simpson to calculate definite integral.
+double dI(double v,double p,double lf,double rt){
+    double md=(lf+rt)/2;
+    double nws=(rt-lf)*(cirf(v,p,lf)+cirf(v,p,rt)+4*cirf(v,p,(lf+rt)/2));
+    double lfs=(md-lf)*(cirf(v,p,lf)+cirf(v,p,md)+4*cirf(v,p,(lf+md)/2));
+    double rts=(rt-md)*(cirf(v,p,md)+cirf(v,p,rt)+4*cirf(v,p,(md+rt)/2));
+    if(fabs(lfs+rts-nws)<EPS) return nws;
+    else return dI(v,p,lf,md)+dI(v,p,md,rt);
+}
+
+//definite Integral (not simpson!).
+double dI(int bd){
+    if(!type[bd]){
+        double lbf=bod[bd]*k[bd]+b[bd],rbf=bod[bd+1]*k[bd]+b[bd];
+        return (lbf+rbf)*(bod[bd]-bod[bd+1])/2.0;
+    }else{
+        // calculate function y=sqrt(v[bd]^2-(x-p[bd])^2) from bod[bd] to bod[bd+1].
+        return dI(v[bd],p[bd],bod[bd],bod[bd+1]);
+    }
+}
 int main(){
-    int n;
-    double alpha,h[SIZE],r[SIZE];
-    double pjx[SIZE];
-    double ans=0;
-
-    // true means the circle, false means the line (function).
-    bool type[SIZE];
-    // board of functions, for final INTEGRATION.
-    double bod[SIZE];
-    // if we uses circle function y=sqrt(v^2-(x-p)^2), we must record v and the p.
-    double v[SIZE],p[SIZE];
-    // if we uses the line function y=kx+b, we must record k and b.
-    double k[SIZE],b[SIZE];
-    // number of functions(in boards).
-    int cnt;
-
     cin>>n>>alpha;
     for(int i=0;i<=n;i++) cin>>h[i];
     for(int i=1;i<=n;i++) cin>>r[i];
@@ -58,8 +87,9 @@ int main(){
         bod[2]=x;
         k[1]=-p1/(h[0]-x);
         b[1]=(p1*h[0])/(h[0]-x);
-        // we don't know if it is right.
+        cout<<dI(1)<<endl;
     }
     cnt=1;
+    //bye, hope i can solve this problem tomorrow :-)
     return 0;
 }
