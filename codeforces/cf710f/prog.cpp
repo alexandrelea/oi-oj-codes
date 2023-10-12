ea0a3aa3268622efd5a9c252ac0c3775a9720012
Littlelu <3287597970@qq.com>
2023-10-12 19:59:21 +0800

    Finally AC code of CF710F!!!

--- codeforces/cf710f/prog.cpp
+++ codeforces/cf710f/prog.cpp
@@ -3,76 +3,69 @@
 #include <string>
 #include <cmath>
 using namespace std;
+const int SIZE=6e5+10;
 struct ACAM{
     const static int SIGMA=26;
-    vector<vector<int>> trans,bktr;
-    vector<int> fail,ed;
-    bool changed,got;
-    int tot,root=0,tans=0;
-    ACAM(int ze){
-        fail=vector<int>((ze+1)*SIGMA,0),ed=vector<int>((ze+1)*SIGMA);
-        //fail.resize(ze+1),ed.resize(ze+1);
-        bktr=trans=vector<vector<int>>((ze+1)*SIGMA,vector<int>(SIGMA,0));
-        // trans.resize(ze+1,vector<int>(SIGMA));
-        root=tot=0,changed=got=false;
-    }
-    void insert(string st){
-        if(!changed) trans=bktr;
+    int cnt,rts,trans[SIZE][SIGMA],tarns[SIZE][SIGMA];
+    int root[SIZE],fail[SIZE],val[SIZE],siz[SIZE];
+    long long tmp[SIZE];
+    void insert(string s,int &root){
+        if(root==0) root=++cnt;
         int co=root;
-        for(auto ch:st){
-            if(trans[co][ch-'a']==0) trans[co][ch-'a']=(++tot);
+        for(auto ch:s){
+            if(trans[co][ch-'a']==0) trans[co][ch-'a']=++cnt;
             co=trans[co][ch-'a'];
         }
-        ed[co]++,changed=true,got=false;
+        val[co]++;
     }
-    void build(){
-        if(!changed) return;
-        changed=false,got=false;
-        bktr=trans,fail=vector<int>(fail.size(),0);
+    void build(int root){
         queue<int> que;
-        // for(int i=0;i<SIGMA;i++) trans[0][i]=1;
-        // que.push(1),fail[1]=0;
-        for(int i=0;i<SIGMA;i++) if(trans[root][i]!=0) que.push(trans[root][i]);
+        fail[root]=root;
+        for(int i=0;i<SIGMA;i++){
+            if(trans[root][i]!=0) tarns[root][i]=trans[root][i],fail[trans[root][i]]=root,que.push(trans[root][i]);
+            else tarns[root][i]=root;
+        }
         while(!que.empty()){
             int co=que.front();que.pop();
-            for(int i=0;i<SIGMA;i++){
-                if(trans[co][i]) fail[trans[co][i]]=trans[fail[co]][i],que.push(trans[co][i]);
-                else trans[co][i]=trans[fail[co]][i];
+            tmp[co]=val[co]+tmp[fail[co]];
+            for(int i=0;i<SIGMA;i++)
+                if(trans[co][i]) tarns[co][i]=trans[co][i],fail[trans[co][i]]=tarns[fail[co]][i],que.push(trans[co][i]);
+                else tarns[co][i]=tarns[fail[co]][i];
+        }
+    }
+    int merge(int x,int y){
+        if(x==0||y==0) return x+y;
+        val[x]+=val[y];
+        for(int i=0;i<SIGMA;i++) trans[x][i]=merge(trans[x][i],trans[y][i]);
+        return x;
+    }
+    void insert(string st){
+        siz[++rts]=1;
+        insert(st,root[rts]);
+        while(siz[rts-1]==siz[rts]) siz[rts-1]+=siz[rts],root[rts-1]=merge(root[rts-1],root[rts]),root[rts]=0,rts--;
+        build(root[rts]);
+    }
+    long long query(string st){
+        long long ans=0;
+        for(int i=1;i<=rts;i++){
+            int co=root[i];
+            for(auto ch:st){
+                co=tarns[co][ch-'a'];
+                ans+=tmp[co];
             }
         }
+        return ans;
     }
-    int query(string st){
-        if(got) return tans;
-        got=true;
-        int co=root,ans=0;
-        for(auto ch:st){
-            co=trans[co][ch-'a'];
-            for(int nw=co;nw!=0;nw=fail[nw]) ans+=ed[nw];
-        }
-        return tans=ans;
-    }
-}inserted[13]={ACAM(2),ACAM(4),ACAM(8),ACAM(16),ACAM(32),ACAM(64),ACAM(128),ACAM(256),ACAM(512),ACAM(1024),ACAM(2048),ACAM(4096),ACAM(8192)},
-deleted[13]={ACAM(2),ACAM(4),ACAM(8),ACAM(16),ACAM(32),ACAM(64),ACAM(128),ACAM(256),ACAM(512),ACAM(1024),ACAM(2048),ACAM(4096),ACAM(8192)};
+}in,de;
 int main(){
     int m,t;
     string st;
     cin>>m;
     while(m--){
         cin>>t>>st;
-        if(t==1||t==2){
-            int lon=log2(st.length());
-            if(t==1) inserted[lon].insert(st);
-            else deleted[lon].insert(st);
-        }else{
-            int ins=0,del=0;
-            for(int i=0;i<13;i++){
-                inserted[i].build();
-                deleted[i].build();
-                ins+=inserted[i].query(st);
-                del+=deleted[i].query(st);
-            }
-            cout<<ins-del<<endl<<flush;
-        }
+        if(t==1) in.insert(st);
+        if(t==2) de.insert(st);
+        if(t==3) cout<<in.query(st)-de.query(st)<<endl<<flush;
     }
     return 0;
 }
