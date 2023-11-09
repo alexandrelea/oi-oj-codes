6a2230f0346bf0526e7d9c0f63bbe3ef746ff875
Littlelu <3287597970@qq.com>
2023-11-09 09:25:30 +0800

    AC program of P2758.

---
+++ luogu/p2758/prog.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int min(int x, int y, int z)
+{
+    return min(min(x, y), z);
+}
+int edit_dist_dp(string str1, string str2)
+{
+    int m = str1.length();
+    int n = str2.length();
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+    for (int i = 0; i <= m; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (i == 0)
+                dp[i][j] = j;
+            else if (j == 0)
+                dp[i][j] = i;
+            else if (str1[i - 1] == str2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1];
+            else
+                dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]);
+        }
+    }
+    return dp[m][n];
+}
+int main()
+{
+    string str1, str2;
+    cin >> str1 >> str2;
+    cout << edit_dist_dp(str1, str2) << endl;
+    return 0;
+}
