4ecd1f28a9e6486b9449bdc650f3dfbb9feb2e67
Littlelu <littlelu.wang@foxmail.com>
2023-12-17 00:48:05 +0800

    AC program of CF1905A.

---
+++ codeforces/cf1905a/prog.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t,n,m;
+    cin>>t;
+    while(t--){
+        cin>>n>>m;
+        cout<<max(n,m)<<endl;
+    }
+    return 0;
+}
