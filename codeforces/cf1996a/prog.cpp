b56ab2db2f54a4587be2b0403bfd0322c63ec6d7
Littlelu <littlelu.wang@foxmail.com>
2024-08-01 21:56:34 +0800

    AC program of CF1996A.

---
+++ codeforces/cf1996a/prog.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int _;
+    cin>>_;
+    while(_--){
+        int n;
+        cin>>n;
+        cout<<(n/4)+(n%4)/2<<endl;
+    }
+    return 0;
+}
