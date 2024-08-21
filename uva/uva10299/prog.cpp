65b25e413f1cbc446a3892b897fdd8d78577e00b
Alexandre Lea <3287597970@qq.com>
2024-08-21 22:11:12 +0800

    AC code of UVa10299.

---
+++ uva/uva10299/prog.cpp
@@ -0,0 +1,21 @@
+// UVa10299 - Relatives
+// Code by Alexandre Lea
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+void solve(){
+    int phi=1,k=n;
+    for(int i=2;i*i<=n;++i){
+        if(k%i==0){
+            k/=i,phi*=(i-1);
+            while(k%i==0) k/=i,phi*=i;
+        }
+    }
+    if(k!=1) phi*=(k-1);
+    if(n==1) phi=0;
+    cout<<phi<<endl;
+}
+int main(){
+    while(cin>>n&&n) solve();
+    return 0;
+}
